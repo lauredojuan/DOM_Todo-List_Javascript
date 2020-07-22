585ce763386995583320a70ff879d4179a5c7d26
Juan L <lauredojuan@gmail.com>
2020-07-22 13:37:44 +0000

    first commit

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,21 @@
-// Your code here
+let liList = ["Eat", "Drink", "Sleep"];
+let sList = liList.map(function(li) {
+    return '<li><span><i class="fa fa-trash"></i></span>' + li + "</li>";
+});
+function keyupfunct(event) {
+    if (event.keyCode === 13) {
+        liList.push(event.target.value);
+        sList = liList.map(function(li) {
+            return '<li><span><i class="fa fa-trash"></i></span>' + li + "</li>";
+        });
+        document.querySelector("#ulElem").innerHTML = sList.join(" ");
+        document.querySelector("#addToDo").value = "";
+    }
+}
+console.log(sList);
+document.querySelector("#ulElem").innerHTML = sList.join(" ");
+document.querySelector("#addToDo").addEventListener("keyup", keyupfunct);
+document.querySelector("li").addEventListener("click", keyupfunct);
+// liList.map(function(li) {
+//  document.querySelector("#ulElem").innerHTML = li;
+// });
